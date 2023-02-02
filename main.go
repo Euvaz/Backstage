959083c6976d718ae975e81c43b7da00d7e8e24a
euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2023-02-01 22:08:45 -0600

    feat: Expanded code structure

--- main.go
+++ main.go
@@ -2,18 +2,17 @@ package main
  
 import (
     "database/sql"
+    "encoding/base64"
     "log"
     "os"
     "fmt"
     _ "github.com/jackc/pgx/v5/stdlib"
+    "github.com/spf13/viper"
+    "math/rand"
 )
  
 const (
-    dbHost     = "localhost"
-    dbPort     = 5432
-    dbUser     = "user"
-    dbPass     = "pass"
-    dbName     = "db"
+    alphanum = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 )
 
 func initTables(db *sql.DB) {
@@ -40,10 +39,29 @@ func initTableTokens(db *sql.DB) {
     log.Printf("Creating \"tokens\" table if not alrady present...")
     var execStr string = `CREATE TABLE IF NOT EXISTS tokens (
                             id SERIAL PRIMARY KEY,
+                            name TEXT,
                             created TIMESTAMP
                           )`
     db.Exec(execStr)
     log.Printf("Success")
+}
+
+func RandStringBytes(n int) string {
+    b := make([]byte, n)
+    for i := range b {
+        b[i] = alphanum[rand.Intn(len(alphanum))]
+    }
+    return string(b)
+}
+
+func genEnrollmentToken(db *sql.DB, host string, port int) {
+    var enrollmentToken string = RandStringBytes(50)
+    var enrollmentTokenJSON string = base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("{addr:\"%s:%v\",token:\"%s\"}", host, port, enrollmentToken)))
+    var execStr string = fmt.Sprintf(`INSERT INTO tokens (id, name, created)
+                                      VALUES (DEFAULT, '%s', CURRENT_TIMESTAMP)`,
+                                      enrollmentToken)
+    db.Exec(execStr)
+    log.Printf("Token Generated")
 }
 
 func enrollNode(db *sql.DB) {
@@ -51,18 +69,23 @@ func enrollNode(db *sql.DB) {
     var nodePort int = 3802
     var nodeName string = "node-1"
     var execStr string = fmt.Sprintf(`INSERT INTO nodes (id, address, port, name)
-                                      VALUES (DEFAULT, '%s', %v, %s)`,
+                                      VALUES (DEFAULT, '%s', %v, '%s')`,
                                       nodeAddress, nodePort, nodeName)
     db.Exec(execStr)
-    log.Printf("Node %s enrolled", nodeName)
+    log.Printf("Node \"%s\" enrolled", nodeName)
 }
 
 func main() {
     log.SetFlags(log.Lshortfile)
     log.SetPrefix("Backstage: ")
 
+    vi := viper.New()
+    vi.SetConfigFile("config.yaml")
+    vi.ReadInConfig()
+
     psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-                             dbHost, dbPort, dbUser, dbPass, dbName)
+                             vi.GetString("dbHost"), vi.GetInt("dbPort"), vi.GetString("dbUser"),
+                             vi.GetString("dbPass"), vi.GetString("dbName"))
 
     log.Printf("Connecting to database...")
     db, err := sql.Open("pgx", psqlconn)
@@ -71,6 +94,7 @@ func main() {
         os.Exit(1)
     }
     defer db.Close()
+    defer log.Printf("Database connection closed")
 
     err = db.Ping()
     if err != nil {
@@ -80,4 +104,6 @@ func main() {
     log.Printf("Connection established")
 
     initTables(db)
+
+    //genEnrollmentToken(db, vi.GetString("host"), vi.GetInt("port"))
 }
