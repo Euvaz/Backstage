f876abac96046c04c6df20cd1093d0b22b2f72ac
euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2023-02-05 16:45:13 -0600

    feat: Re-add code for token generation function

--- main.go
+++ main.go
@@ -159,3 +159,14 @@ func closeDB(db *sql.DB) {
 //    db.Exec(execStr)
 //    logger.Infof("drone \"%s\" Enrolled", droneName)
 //}
+
+// Function to generate an enrollment token
+//func genEnrollmentToken(db *sql.DB, host string, port int) {
+//    var key string = RandStringBytes(50)
+//    var enrollmentToken string = base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf(`{"addr":"%s:%v","key":"%s"}`, host, port, key)))
+//    var execStr string = fmt.Sprintf(`INSERT INTO tokens (id, key, created)
+//                                      VALUES (DEFAULT, '%s', CURRENT_TIMESTAMP)`, key)
+//    db.Exec(execStr)
+//    log.Printf("Generated Token: \"%s\"", enrollmentToken)
+//    log.Printf("Generated Key: \"%s\"", key)
+//}
