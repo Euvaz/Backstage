23404cb086050d3b08d0d01fb54ad3f0ade333b6
Euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2023-02-07 18:41:10 -0600

    feat: Add ability to read drone names and keys

--- routing.go
+++ routing.go
@@ -2,25 +2,38 @@ package main
 
 import (
 	"database/sql"
+    "encoding/json"
 	"io/ioutil"
-    "fmt"
 	_ "net/http"
 
 	"github.com/Euvaz/Backstage-Hive/logger"
 	"github.com/gin-gonic/gin"
 )
 
+type Token struct {
+    Key string  `json:"key"`
+}
+
 func registerRoutes (router *gin.Engine, db *sql.DB) {
-    router.GET("/drones", func(ctx *gin.Context) {
+    router.GET("/drones/:name", func(ctx *gin.Context) {
         logger.Info("Handling GET /drones")
     })
 
-    router.POST("/drones", func(ctx *gin.Context) {
+    router.POST("/drones/:name", func(ctx *gin.Context) {
         logger.Info("Handling POST /drones")
+        name := ctx.Param("name")
+        logger.Info(name)
+
         jsonData, err := ioutil.ReadAll(ctx.Request.Body)
         if err != nil {
             logger.Error(err.Error())
         }
-        fmt.Println(string(jsonData))
+
+        var token Token
+        err = json.Unmarshal(jsonData, &token)
+        if err != nil {
+            logger.Fatal(err.Error())
+        }
+        logger.Info(string(token.Key))
     })
 }
